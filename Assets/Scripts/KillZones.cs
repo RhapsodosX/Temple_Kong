c6e26d94dede8f931b39001c007feebb3278e1da
Rhaps <naruga@knights.ucf.edu>
2018-02-16 04:51:10 -0500

    v5

---
+++ Assets/Scripts/KillZones.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillZones : MonoBehaviour {
+	public CircleCollider2D collider;
+	public GameObject player;
+	// Use this for initialization
+	void Start () {
+		collider = GetComponent<CircleCollider2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnTriggerEnter2D(Collider2D collider){
+		player.GetComponent<Player> ().playerState = Player.PlatformerState.Dead;
+	}
+}
---
+++ Assets/Scripts/PickUps.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUps : MonoBehaviour {
+
+	public CircleCollider2D circleCollider;
+	public BoxCollider2D boxCollider;
+	private int score;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Destroy (gameObject);
+		score = PlayerPrefs.GetInt ("PlayerScore");
+		score += 800;
+		PlayerPrefs.SetInt ("PlayerScore", score);
+	}
+}
